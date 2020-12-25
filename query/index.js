8ff9048c3bcfa14df7541f83b44cb747fac6d84c
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-25 19:35:44 +0530

    changed port

--- query/index.js
+++ query/index.js
@@ -4,10 +4,23 @@ const app = express();
 app.use(bodyParser.json());
 
 const posts = {};
-app.get('/posts', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
 
-app.get('/events', (req, res) => {});
+app.get('/events', (req, res) => {
+  const { type, data } = req.body;
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+});
 
-app.listen(4003, () => {
-  console.log('App Listening on port 4003');
+app.listen(4002, () => {
+  console.log('App Listening on port 4002');
 });
