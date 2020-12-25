c0d07f7cb030d9264127e3c70d55520e797e12e6
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-25 15:34:41 +0530

    Added node service implementation

--- posts/index.js
+++ posts/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
 const app = express();
-app.get('/posts', (req, res) => {});
+app.use(bodyParser.json());
 
-app.post('/posts', (req, res) => {});
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  post[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
   console.log('Listening on http 4000');
