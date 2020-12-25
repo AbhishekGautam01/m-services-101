926a51648165a1be75408374eab3cb7b188cdd44
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-25 16:41:07 +0530

    Post List

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
+
 function App() {
   return (
     <div className="container">
       Blog App
       <h1>Post Create</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 }
