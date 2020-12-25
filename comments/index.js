9ce9362a30f8ce4da800dbf54dc6100d8ce821b5
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-25 16:06:45 +0530

    IMplementing Comment Service

---
+++ comments/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const app = express();
+app.use(bodyParser.json());
+
+//{IdofPost: [array of comment]}
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+  const comment = commentsByPostId[req.params.id] || [];
+  comment.push({ id: commentId, content: content });
+  commentsByPostId[req.params.id] = comment;
+
+  res.status(201).send(comment);
+});
+
+app.listen(4001, () => {
+  console.log('Listening for 4001');
+});
