c43c63e5db9ce1e15b6aec3b45cd3a3b7116292d
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-25 19:22:26 +0530

    Added Event Handlers

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const { default: axios } = require('axios');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -13,14 +14,26 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
   const comment = commentsByPostId[req.params.id] || [];
   comment.push({ id: commentId, content: content });
   commentsByPostId[req.params.id] = comment;
-
+  await axios.post('http://localhost/:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
   res.status(201).send(comment);
+});
+
+app.post('/events', (req, res) => {
+  console.log(`recivieved event ${req.body.type}`);
+  res.send({});
 });
 
 app.listen(4001, () => {
